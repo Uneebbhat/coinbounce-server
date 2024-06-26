ab335a9e5faf4c7a93bb67c7db40f09c7cc6c3ce
Uneeb <uneebbhatti3@gmail.com>
2024-06-26 19:50:40 +0500

    User model created

---
+++ src/models/User.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+
+const userSchema = new Schema(
+  {
+    profilePic: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: [5, "Name must be at least 5 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      validate: [validator.isEmail, "Invalid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
